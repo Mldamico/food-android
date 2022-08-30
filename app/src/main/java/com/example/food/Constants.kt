93690cfa2c99e8014fcd408f8c54fe116e6b8178
mldamico <matias.lioneldamico@gmail.com>
2022-08-30 15:28:53 -0300

    Constant and Recipe api interface

---
+++ app/src/main/java/com/example/food/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.food
+
+class Constants {
+    companion object {
+        const val API_KEY="1a1f8b70e58f42bb814c77331a5f9daa"
+        const val BASE_URL=" https://api.spoonacular.com"
+    }
+}
--- app/src/main/java/com/example/food/ui/MainActivity.kt
+++ app/src/main/java/com/example/food/ui/MainActivity.kt
@@ -1,13 +1,13 @@
-package com.example.food
+package com.example.food.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.food.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
