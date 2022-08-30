c2d4be3fbba3ff5b783605b482567243eedf3c1e
mldamico <matias.lioneldamico@gmail.com>
2022-08-30 18:45:18 -0300

    Added application

--- app/src/main/java/com/example/food/Constants.kt
+++ app/src/main/java/com/example/food/Constants.kt
@@ -2,7 +2,7 @@ package com.example.food
 
 class Constants {
     companion object {
-        const val API_KEY="1a1f8b70e58f42bb814c77331a5f9daa"
+        const val API_KEY="f018e48a65ab476a918a76f9444eaf6f"
         const val BASE_URL=" https://api.spoonacular.com"
     }
 }
