85bfe8fb12dbc9585a4cac50caecd787dcf405a3
mldamico <matias.lioneldamico@gmail.com>
2022-09-07 17:38:34 -0300

    added search icon

--- app/src/main/java/com/example/food/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/food/ui/fragments/recipes/RecipesFragment.kt
@@ -2,10 +2,9 @@ package com.example.food.ui.fragments.recipes
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -27,7 +26,7 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class RecipesFragment : Fragment() {
+class RecipesFragment : Fragment(), SearchView.OnQueryTextListener {
     private val args by navArgs<RecipesFragmentArgs>()
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
@@ -49,6 +48,7 @@ class RecipesFragment : Fragment() {
         _binding =  FragmentRecipesBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.mainViewModel = mainViewModel
+        setHasOptionsMenu(true)
 
 //        view.recyclerview.showShimmer()
         setupRecyclerView()
@@ -80,6 +80,23 @@ class RecipesFragment : Fragment() {
         binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
         showShimmerEffect()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.recipes_menu, menu)
+        val search = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        return true
+    }
+
 
     private fun readDatabase() {
 
