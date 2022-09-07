44a746ccf80d0d9b89ad3c7b225dfedd5eccf1f8
mldamico <matias.lioneldamico@gmail.com>
2022-09-07 18:33:42 -0300

    Activity details basic style

---
+++ app/src/main/java/com/example/food/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.food.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.food.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
