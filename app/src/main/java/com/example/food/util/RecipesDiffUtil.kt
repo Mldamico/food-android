0f222844c9e999ba92f2694a0dcbcb3692961e5e
mldamico <matias.lioneldamico@gmail.com>
2022-09-08 19:36:35 -0300

    Change diffutil to accept generics

--- app/src/main/java/com/example/food/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/example/food/util/RecipesDiffUtil.kt
@@ -3,7 +3,7 @@ package com.example.food.util
 import androidx.recyclerview.widget.DiffUtil
 import com.example.food.models.Result
 
-class RecipesDiffUtil(private val oldList: List<Result>, private val newList: List<Result>): DiffUtil.Callback() {
+class RecipesDiffUtil<T>(private val oldList: List<T>, private val newList: List<T>): DiffUtil.Callback() {
     override fun getOldListSize(): Int {
         return oldList.size
     }
